fdfc19b55eb8e2f9a1cb44b276cafc726d74b42b
vicente_nascimento <vicente19981@live.com>
2023-08-12 13:42:00 -0300

    feat: user can clap and unclap

--- core_apps/articles/models.py
+++ core_apps/articles/models.py
@@ -10,6 +10,20 @@ from .read_time_engine import ArticleReadTimeEngine
 User = get_user_model()
 
 
+class Clap(TimeStampedModel):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="claps")
+
+    article = models.ForeignKey("Article", on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ["user", "article"]
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return f"{self.user.first_name} clapped {self.article.title}"
+
+
 class Article(TimeStampedModel):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="articles"
@@ -24,6 +38,9 @@ class Article(TimeStampedModel):
         "banner image"), default="/profile_default.png")
 
     tags = TaggableManager()
+
+    claps = models.ManyToManyField(
+        User, through=Clap, related_name="clapped_articles")
 
     def __str__(self) -> str:
         return f"{self.author.first_name}'s article"
